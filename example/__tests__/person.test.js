d6071200a912024a814ca86e4e66b00e06a36cce
AndreiCalazans <andreixoc@hotmail.com>
2018-03-22 18:57:46 -0400

    fix(test): typo

--- example/__tests__/person.test.js
+++ example/__tests__/person.test.js
@@ -12,15 +12,13 @@ const query = `
   }
 `;
 
-describe('Person query', () => {
-  it('Check recived object shape', async () => {
-    const response = await graphql(schema, query);
+it('received object shape', async () => {
+  const response = await graphql(schema, query);
 
-    expect(response.data.persons[0]).toMatchObject({
-      uid: expect.any(String),
-      personId: expect.any(Number),
-      name: expect.any(String),
-      __typename: expect.any(String),
-    });
+  expect(response.data.persons[0]).toMatchObject({
+    uid: expect.any(String),
+    personId: expect.any(Number),
+    name: expect.any(String),
+    __typename: expect.any(String),
   });
 });
